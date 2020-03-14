b7dde09def991fcc307400f522c9312ad25cb4f2
Filip Maciejowski <filip.maciejowski@gmail.com>
2020-03-14 22:45:59 +0100

    redux accepted change

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,13 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { userReducer } from "./reducers/userReducer";
-<<<<<<< HEAD
 import { formDataReducer } from "./reducers/formDataReducer";
 import thunk from "redux-thunk";
-=======
-import { formDataReducer } from "./reducers/formDataReducer"
-import thunk from 'redux-thunk';
->>>>>>> we_help
 
 const rootReducer = combineReducers({
   userData: userReducer,
